2c85e131198cc90841205af29fcc124afb41b764
Puja-C <86889969+Puja-C@users.noreply.github.com>
2023-03-21 22:11:28 +0530

    Add files via upload

---
+++ Word replacement.py
@@ -0,0 +1,10 @@
+def replace_word():
+
+    str = "My favourite colour is Pink!"
+    word_to_replace = input("Enter the word to replace:")
+    word_to_replace_with = input("Enter the word to be replaced with:")
+
+    print(str.replace(word_to_replace,word_to_replace_with))
+
+
+replace_word()
